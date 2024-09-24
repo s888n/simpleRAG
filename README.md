f1e15861f03ff9d9304107a9099a599625e5e8da
sss <117085422+s888n@users.noreply.github.com>
2024-09-24 14:58:25 +0100

    containerized

---
+++ README.md
@@ -0,0 +1,57 @@
+# RAG (Retrieval Augmented Generation)
+
+## 1. What is RAG?
+
+- [best explanation of RAG](https://www.youtube.com/watch?v=u47GtXwePms)
+
+## 2. What are Embeddings?
+
+**What are embeddings**
+Text embeddings are a natural language processing (NLP) technique that converts text into numerical coordinates (called vectors) that can be plotted in an n-dimensional space. This approach lets you treat pieces of text as bits of relational data, which we can then train models on.
+
+**Example**
+
+Embeddings capture semantic meaning and context which results in text with similar meanings having closer embeddings. For example, the sentence "I took my dog to the vet" and "I took my cat to the vet" would have embeddings that are close to each other in the vector space since they both describe a similar context.
+
+**How the simularity is calculated**
+
+Now that the embeddings are generated, let's create a Q&A system to search these documents. You will ask a question about hyperparameter tuning, create an embedding of the question, and compare it against the collection of embeddings in the dataframe.
+The embedding of the question will be a vector (list of float values), which will be compared against the vector of the documents using the dot product. This vector returned from the API is already normalized. The dot product represents the similarity in direction between two vectors.
+The values of the dot product can range between -1 and 1, inclusive. If the dot product between two vectors is 1, then the vectors are in the same direction. If the dot product value is 0, then these vectors are orthogonal, or unrelated, to each other. Lastly, if the dot product is -1, then the vectors point in the opposite direction and are not similar to each other.
+
+## 3 How to run
+Pre-requisites: docker , docker compose
+
+1 - Clone the repo and cd into the directory
+```bash 
+    git clone https://github.com/s888n/simpleRAG.git
+    cd simpleRAG
+```
+
+2 - Create a .env file and add GEMINI_API_KEY=your_api_key (you can get an api key from [here](https://aistudio.google.com/app/apikeys))
+3 - run the app
+```bash
+    docker compose up
+```
+**That's it**: you can now access the app at http://localhost:8501
+
+
+## stuff i should learn about
+
+- [the math behind tokens to vectors](https://medium.com/@amallya0523/how-an-llm-understands-input-the-math-under-the-hood-114ac69f96c6)
+- [Huggingface](https://huggingface.co/docs)
+- optimal chunk size
+
+## Resources
+
+- [google gemini API](https://ai.google.dev/gemini-api/docs)
+- [about RAG](https://arxiv.org/abs/2005.11401)
+- [the paper](https://arxiv.org/pdf/2005.11401)
+- [text splitting strategies](https://github.com/FullStackRetrieval-com/RetrievalTutorials/blob/main/tutorials/LevelsOfTextSplitting/5_Levels_Of_Text_Splitting.ipynb)
+- [NVIDIA article](https://blogs.nvidia.com/blog/what-is-retrieval-augmented-generation)
+
+## Demo
+
+https://github.com/user-attachments/assets/a25d07bd-4778-40cd-8ab5-baf1392118c5
+
+
---
+++ rag/app.py
@@ -0,0 +1,84 @@
+# import altair as alt
+# import numpy as np
+# import pandas as pd
+import os
+import streamlit as st
+import chromadb
+from langchain_community.document_loaders import PyPDFLoader
+import tempfile
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import chromadb.utils.embedding_functions as embedding_functions
+from langchain_community.embeddings.sentence_transformer import (
+    SentenceTransformerEmbeddings,
+)
+from langchain_chroma import Chroma
+import google.generativeai as genai
+
+# innitialize the generative model
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
+
+# innitialize the chromadb client
+chroma_client = chromadb.HttpClient(host="chromadb", port=8000, ssl=False)
+chroma_client.heartbeat()
+embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+
+def create_prompt(question, document):
+    return f"You are a helpful and informative bot that answers questions using text from the reference passage included below. \
+Be sure to respond in a complete sentence, being comprehensive, including all relevant background information. \
+However, you are talking to a non-technical audience, so be sure to break down complicated concepts and \
+strike a friendly and converstional tone. \
+If the passage is irrelevant to the answer, you may ignore it. \
+QUESTION: '{question}' \
+PASSAGE: '{document}'"
+
+def main():
+
+    st.title("simple RAG ap")
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # print chat history
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # handle the file upload
+    uploaded_file = st.file_uploader(
+        "Choose a PDF file", type="pdf", accept_multiple_files=False
+    )
+    if uploaded_file is not None:
+        tempdir = tempfile.mkdtemp()
+        path = os.path.join(tempdir, uploaded_file.name)
+        with open(os.path.join(tempdir, uploaded_file.name), "wb") as f:
+            f.write(uploaded_file.getvalue())
+        loader = PyPDFLoader(path)
+        docs = loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+        splits = text_splitter.split_documents(docs)
+        # st.write(splits)
+        Chroma.from_documents(
+            documents=splits,
+            embedding=embedding_function,
+            collection_name="test",
+            client=chroma_client,
+        )
+        collection = chroma_client.get_collection(name="test")
+
+    # get the question
+    if prompt := st.chat_input("What do you want to say to your PDF?"):
+        with st.chat_message("user"):
+            st.markdown(prompt)
+            st.session_state.messages.append({"role": "user", "content": prompt})
+        relevent_document = collection.query(query_texts=[prompt], n_results=1)[
+            "documents"
+        ][0][0]
+        answer = model.generate_content(create_prompt(prompt, relevent_document)).text
+        with st.chat_message("bot"):
+            st.markdown(answer)
+            st.session_state.messages.append({"role": "bot", "content": answer})
+
+
+if __name__ == "__main__":
+    main()
